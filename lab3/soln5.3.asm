9c7015bf389df9baff04de939dcdb00779b93698
Brian Gillespie <brian.x.gillespie@dit.ie>
2013-06-22 23:06:20 +0200

    Update soln5.3.asm
    Test count is zero before adding next number

--- lab3/soln5.3.asm
+++ lab3/soln5.3.asm
@@ -9,33 +9,33 @@ main:                                           ;tell linker entry point
     ; Compute the sum
     xor     eax, eax				;initialise accumulator to zero
     mov     ecx, count				;count of numbers in the list
-    mov     esi, nums				;addess of first number
+    mov     esi, nums				;address of first number
 .loop:
+    cmp     ecx, 0    			    ;are we done?
     add     eax, [esi]				;add next in list to total
-    cmp     ecx, 0				;are we done?
-    jz      .done				;yes!
-    dec     ecx					;no, so subtract 1 from count
-    add     esi, 4				;advance to the next number in the list
-    jmp     .loop
+    jz      .done				    ;yes!
+    dec     ecx					    ;no, so subtract 1 from count
+    add     esi, 4				    ;advance to the next number in the list
+    jmp     .loop                   ;go around again
 .done:
 
     ; Compute the average from the sum and count
     mov     ebx, eax				;make a copy of the computed sum
     xor     edx, edx				;clear top half of dividend, eax is lower half
     mov     ecx, count				;divisor is count
-    div     ecx					;get the average (result is placed in eax)
+    div     ecx					    ;get the average (result is placed in eax)
     
     ; Easiest way to print numbers is to call the printf library (from C)
     ; to do this for us
-    push    eax					;third parameter (for printf) is average
-    push    ebx					;second parameter (for printf) is sum
-    push    fmt					;first parameter (for printf) format string
-    call    printf				;call printf function
-    add     esp, 12				;restore the stack frame (removing args)
+    push    eax					    ;third parameter (for printf) is average
+    push    ebx					    ;second parameter (for printf) is sum
+    push    fmt				   	    ;first parameter (for printf) format string
+    call    printf				    ;call printf function
+    add     esp, 12				    ;restore the stack frame (removing args)
     
-    mov     eax, 1                              ;system call number (sys_exit)
-    mov     ebx, 0                              ;program return code (0 means no error)
-    int     0x80                                ;call kernel
+    mov     eax, 1                  ;system call number (sys_exit)
+    mov     ebx, 0                  ;program return code (0 means no error)
+    int     0x80                    ;call kernel
 
 section     .data
 
